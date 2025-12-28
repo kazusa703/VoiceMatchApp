3c0634de501a06dc3dfdc3fe3a5dd582c1a6350e
imai kazusa <kazusa1304@icloud.com>
2025-12-29 07:10:12 +0900

    あ

---
+++ Desktop/VoiceMatchApp/VoiceMatchApp/ui/Flowlayout.swift
@@ -0,0 +1,18 @@
+//
+//  Flowlayout.swift
+//  VoiceMatchApp
+//
+//  Created by 今井一颯 on 2025/12/29.
+//
+
+import SwiftUI
+
+struct Flowlayout: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Flowlayout()
+}
