7343e0add1fb858aa9061e85ae44b2a81890f132
imai kazusa <kazusa1304@icloud.com>
2025-12-29 09:26:23 +0900

    あ

---
+++ Desktop/VoiceMatchApp/VoiceMatchApp/file/Hashtagcategories.swift
@@ -0,0 +1,8 @@
+//
+//  Hashtagcategories.swift
+//  VoiceMatchApp
+//
+//  Created by 今井一颯 on 2025/12/29.
+//
+
+import Foundation
---
+++ Desktop/VoiceMatchApp/VoiceMatchApp/ui/Voicelikerecordingview.swift
@@ -0,0 +1,18 @@
+//
+//  Voicelikerecordingview.swift
+//  VoiceMatchApp
+//
+//  Created by 今井一颯 on 2025/12/29.
+//
+
+import SwiftUI
+
+struct Voicelikerecordingview: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Voicelikerecordingview()
+}
